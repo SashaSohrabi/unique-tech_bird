27f4f886f3dc7d34586fd4a8249b06882315a185
Vitaliy Sakhno <vitaliy.sakhno1@gmail.com>
2018-08-26 12:00:57 +0300

    before pull nunjucks

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ var gulp = require('gulp'),
 
 
 gulp.task('sass', function () {
-    return gulp.src('app/sass/**/*.scss')
+    return gulp.src('app/scss/**/*.scss')
+       .pipe(sass().on('error', sass.logError))
         .pipe(sass())
         .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
         .pipe(gulp.dest('app/css'))
