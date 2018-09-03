56dede569129f96d44191eac57372a78e0fdcda4
sashasohrabi <shb.sohrabi@gmail.com>
2018-09-04 01:21:25 +0300

    scroll up added.

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
 new WOW().init();
 });
+
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 600) {
+        $('.scrollup').fadeIn('slow');
+    } else {
+        $('.scrollup').fadeOut('slow');
+    }
+});
+
+$('.scrollup').click(function(){
+    $("html, body").animate({ scrollTop: 0 },1000);
+    return false;
+});
