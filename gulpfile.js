cf29d1068c6d69e3d98a9c9b11d4a50af169469e
Max <40343045+HolyMiracle@users.noreply.github.com>
2018-09-05 16:48:34 +0300

    Revert "Shb min files"

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,6 @@
 var gulp = require('gulp'),
-    htmlmin = require('gulp-htmlmin'),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
-    csso = require('gulp-csso'),
-    uglify = require('gulp-uglify'),
     imagemin = require('gulp-imagemin'),
     browserSync = require('browser-sync'),
     nunjucksRender = require('gulp-nunjucks-render');
@@ -11,10 +8,9 @@ var gulp = require('gulp'),
 
 gulp.task('sass', function () {
     return gulp.src('app/scss/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
+       .pipe(sass().on('error', sass.logError))
         .pipe(sass())
         .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {cascade: true}))
-        .pipe(csso())
         .pipe(gulp.dest('app/css'))
         .pipe(browserSync.reload({stream: true}))
 });
@@ -25,22 +21,7 @@ gulp.task('image-min', function () {
         .pipe(gulp.dest('app/assets/image-min'))
 });
 
-gulp.task('pages', function () {
-    return gulp.src(['./app/**/*.html'])
-        .pipe(htmlmin({
-            collapseWhitespace: true,
-            removeComments: true
-        }))
-        .pipe(gulp.dest('./app'));
-});
-
-gulp.task('scripts', function() {
-    return gulp.src('./app/js/main.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('./app/js'))
-});
-
-gulp.task('nunjucks', function () {
+gulp.task('nunjucks', function() {
     return gulp.src('app/pages/**/*.+(html|nunjucks)')
         .pipe(nunjucksRender({
             path: ['app/templates']
@@ -57,9 +38,9 @@ gulp.task('browser-sync', function () {
     });
 });
 
-
-gulp.task('default', ['nunjucks', 'browser-sync', 'pages', 'scripts', 'sass'], function () {
+gulp.task('default', ['nunjucks', 'browser-sync', 'sass'], function () {
     gulp.watch('app/scss/**/*.scss', ['sass']);
+    //gulp.watch('app/pages/**/*.+(html|nunjucks)', browserSync.reload);
     gulp.watch('app/*.html', browserSync.reload);
     gulp.watch('app/js/**/*.js', browserSync.reload);
     gulp.watch('app/templates/**/*.html', ['nunjucks']);
