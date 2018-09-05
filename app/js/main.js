b883dcfb5d0f1fba89334483eed7f1476d3d3ba9
sashasohrabi <shb.sohrabi@gmail.com>
2018-09-05 03:17:16 +0300

    sticky header

--- app/js/main.js
+++ app/js/main.js
@@ -16,3 +16,18 @@ $('.scrollup').click(function(){
     $("html, body").animate({ scrollTop: 0 },1000);
     return false;
 });
+
+
+//sticky menu
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
